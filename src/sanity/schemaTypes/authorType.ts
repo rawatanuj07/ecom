696a9f051f48ed8eee40eba36adbeaa6bc5f83e4
Anuj Rawat <neverlandsboi_@Anujs-MacBook-Air.local>
2024-12-13 19:43:04 +0530

    sanity-icon-removed

--- src/sanity/schemaTypes/authorType.ts
+++ src/sanity/schemaTypes/authorType.ts
@@ -1,11 +1,11 @@
-import {UserIcon} from '@sanity/icons'
+// import {UserIcon} from '@sanity/icons'
 import {defineArrayMember, defineField, defineType} from 'sanity'
 
 export const authorType = defineType({
   name: 'author',
   title: 'Author',
   type: 'document',
-  icon: UserIcon,
+  // icon: UserIcon,
   fields: [
     defineField({
       name: 'name',
