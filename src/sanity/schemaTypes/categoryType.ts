376adaab01a3c8525a7eee45b2194d7c071ba6cc
Anuj Rawat <neverlandsboi_@Anujs-MacBook-Air.local>
2024-12-13 19:54:42 +0530

    iconfixed

--- src/sanity/schemaTypes/categoryType.ts
+++ src/sanity/schemaTypes/categoryType.ts
@@ -1,11 +1,11 @@
-import {TagIcon} from '@sanity/icons'
+// import {TagIcon} from '@sanity/icons'
 import {defineField, defineType} from 'sanity'
 
 export const categoryType = defineType({
   name: 'category',
   title: 'Category',
   type: 'document',
-  icon: TagIcon,
+  // icon: TagIcon,
   fields: [
     defineField({
       name: 'title',
