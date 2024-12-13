3e3469c0f3548ec03e09a285f2529494c56fab84
Anuj Rawat <neverlandsboi_@Anujs-MacBook-Air.local>
2024-12-13 19:50:28 +0530

    icons-removed

--- src/sanity/schemaTypes/postType.ts
+++ src/sanity/schemaTypes/postType.ts
@@ -1,11 +1,11 @@
-import {DocumentTextIcon} from '@sanity/icons'
+// import {DocumentTextIcon} from '@sanity/icons'
 import {defineArrayMember, defineField, defineType} from 'sanity'
 
 export const postType = defineType({
   name: 'post',
   title: 'Post',
   type: 'document',
-  icon: DocumentTextIcon,
+  // icon: DocumentTextIcon,
   fields: [
     defineField({
       name: 'title',
