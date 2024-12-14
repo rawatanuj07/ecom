711ef9e63afae0e75c8c3fc148ffd7005acefaea
rawatanuj07 <rawatanuj07@gmail.com>
2024-12-14 22:23:07 +0530

    salesSchema-added

---
+++ src/sanity/schemaTypes/salesType.ts
@@ -0,0 +1,57 @@
+import { IoPricetagsOutline } from "react-icons/io5";
+import { defineField, defineType } from "sanity";
+export const salesType = defineType({
+  name: "sale",
+  title: "Sale",
+  type: "document",
+  icon: IoPricetagsOutline,
+  fields: [
+    defineField({
+      name: "title",
+      title: "Sale Title",
+      type: "string",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "description",
+      title: "text",
+      type: "text",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "discountAmount",
+      title: "Discount Amount",
+      type: "number",
+      description: "Amount off in percentage or fixed value",
+    }),
+    defineField({
+      name: "validUntil",
+      title: "Valid Until",
+      type: "datetime",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "isActive",
+      title: "Is Active",
+      type: "boolean",
+      description: "Toggle to Activate/Deactivate the sale.",
+      initialValue: true,
+    }),
+  ],
+  preview: {
+    select: {
+      title: "title",
+      discoutnAmount: "discoutnAmount",
+      couponCode: "couponcode",
+      isActive: "isActive",
+    },
+    prepare(selection) {
+      const { title, discountAmount, couponCode, isActive } = selection;
+      const status = isActive ? "Active" : "Inactive";
+      return {
+        title,
+        subtitle: `${discountAmount}% off - Code: ${couponCode} - ${status}`,
+      };
+    },
+  },
+});
