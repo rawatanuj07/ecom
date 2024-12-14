a1103eb2427658d4ee2c79a58d7fad4c4a50b703
rawatanuj07 <rawatanuj07@gmail.com>
2024-12-15 00:04:08 +0530

    schemapost-removed

--- src/sanity/structure.ts
+++ src/sanity/structure.ts
@@ -5,13 +5,9 @@ export const structure: StructureResolver = (S) =>
   S.list()
     .title("DeviDeals E-Commerce")
     .items([
-      S.documentTypeListItem("post").title("Posts"),
       S.documentTypeListItem("category").title("Categories"),
-      S.documentTypeListItem("author").title("Authors"),
       S.divider(),
       ...S.documentTypeListItems().filter(
-        (item) =>
-          item.getId() &&
-          !["post", "category", "author"].includes(item.getId()!)
+        (item) => item.getId() && !["category"].includes(item.getId()!)
       ),
     ]);
