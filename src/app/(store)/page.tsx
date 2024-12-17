3df7d6ae5874404ed7e0c35831d67200137a724b
rawatanuj07 <rawatanuj07@gmail.com>
2024-12-17 23:37:09 +0530

    legacy

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
     <>
       <SaleBanner />
       <UserIcon />
-      <Button>click-me</Button>
+      <Button>click-mei</Button>
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductsView products={products} categories={categories} />
       </div>
