e788cbb4d95808889c0017e89e44757148e465cc
rawatanuj07 <rawatanuj07@gmail.com>
2024-12-18 17:12:08 +0530

    linkdIn

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,9 +1,7 @@
 import ProductsView from "@/components/ProductsView";
-import { Button } from "@/components/ui/button";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
 
-import { UserIcon } from "@sanity/icons";
 import SaleBanner from "@/components/salebanner";
 
 export default async function Home() {
@@ -12,8 +10,6 @@ export default async function Home() {
   return (
     <>
       <SaleBanner />
-      <UserIcon />
-      <Button>click-mei</Button>
       <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
         <ProductsView products={products} categories={categories} />
       </div>
