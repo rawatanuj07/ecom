d6075be8325675d7d87621f5845d9aa6c0a88234
rawatanuj07 <rawatanuj07@gmail.com>
2024-12-29 04:19:49 +0530

    bg-gradient

--- src/app/(store)/categories/[slug]/page.tsx
+++ src/app/(store)/categories/[slug]/page.tsx
@@ -13,7 +13,7 @@ async function CategoryPage(props: { params: Promise<{ slug: string }> }) {
   return (
     <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4">
       <div className=" bg-white p-8 rounded-lg shadow-md w-full max-w-4x1">
-        <h1 className="text-3xl font-bold mb-6 text-center">
+        <h1 className="bg-gradient-to-r from-red-600 to-black text-3xl font-bold mb-6 text-center">
           {slug
             .split("-")
             .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
